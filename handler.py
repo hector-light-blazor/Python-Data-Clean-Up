546ed142982ff289457a407cca6f7f2af0bf2b5b
Hector Chapa <roensebastian2015@gmail.com>
2023-01-28 21:59:54 -0600

    improvements for parsing other field columns

--- handler.py
+++ handler.py
@@ -1,11 +1,15 @@
-import arcpy
+import arcpy, numpy
+
+LegacyStreetType = []
+
 class AddressParser:
     # init constructor
-    def __init__(self, objectid, fullRoadName, fieldData):
+    def __init__(self, objectid, fullRoadName, preDir="", preType=""):
         self.roadName = fullRoadName.strip()
         self.splitRoadArr = fullRoadName.split()
         self.splitLen = len(self.splitRoadArr)
-        self.fieldData = fieldData.strip() if fieldData is not None else ""
+        self.preDir = preDir.strip() if preDir is not None else ""
+        self.preType = preType.strip() if preType is not None else ""
         self.objectid = objectid
         self.hasData = len(self.roadName) > 1
         self.isEmpty = len(self.roadName) == 0
@@ -18,10 +22,11 @@ class AddressParser:
     
     def hasSingle(self):
         hasPrefix = ['N', 'S', 'E', 'W']
-        return self.hasData and len(self.splitRoadArr[0]) == 1 and len(self.fieldData) == 0 and self.splitRoadArr[0] in hasPrefix
+        return self.hasData and len(self.splitRoadArr[0]) == 1 and len(self.preDir) == 0 and self.splitRoadArr[0] in hasPrefix
     
     def hasStType(self):
-        return self.hasData and len(self.splitRoadArr[self.splitLen - 1]) >= 2 and len(self.fieldData) == 0
+        global LegacyStreetType
+        return self.hasData and  len(self.preType) == 0 and self.splitRoadArr[self.splitLen - 1] in LegacyStreetType
     
     def getIsEmpty(self):
         return self.isEmpty
@@ -31,8 +36,33 @@ class AddressParser:
             return self.splitRoadArr[0]
     
     def getStType(self):
-        if self.hasData and len(self.splitRoadArr[self.splitLen - 1]) >= 2:
+        if self.hasData:
             return self.splitRoadArr[self.splitLen - 1]
+        
+    def getRoadName(self):
+        results = ""
+        if len(self.preType) > 0 and len(self.preDir) > 0:
+            start = 1
+            end = self.splitLen - 1
+            for index in range(start, end):
+                results += self.splitRoadArr[index] + " "
+        elif len(self.preType) == 0 and len(self.preDir) == 0:
+            start = 0
+            end = self.splitLen
+            for index in range(start, end):
+                results += self.splitRoadArr[index] + " "
+        elif len(self.preType) > 0 and len(self.preDir) == 0:
+            start = 1
+            end = self.splitLen
+            for index in range(start, end):
+                results += self.splitRoadArr[index] + " "
+        elif len(self.preType) == 0 and len(self.preDir) > 0:
+            start = 0
+            end = self.splitLen - 1
+            for index in range(start, end):
+                results += self.splitRoadArr[index] + " "
+        return results.strip()
+                
             
              
 
@@ -40,9 +70,15 @@ def test():
     hello = AddressParser("1", "none", None)
     print(hello.getField())
 
-def processData(data):
-    return AddressParser(data[1], data[0], data[2])
+def processPreDirData(data):
+    return AddressParser(data[1], data[0], data[2], "")
 
+def processPreTypeData(data):
+    return AddressParser(data[1], data[0],"", data[2])
+
+
+def processRoadData(data):
+    return AddressParser(data[0], data[1],data[2], data[3])
 
 def findValue(arr, item):
     for i, val in enumerate(arr):
@@ -50,13 +86,47 @@ def findValue(arr, item):
             return i, val.getStType()
     return None, None
 
+def findRoadName(arr, item):
+    for i, val in enumerate(arr):
+        if val.getObjectId() == item:
+            return i, val.getRoadName()
+    return None, None
+
 def findTest():
     return None, None
 
 
-def startProcess(fs, fields):
-    prefixDirValuesDirty = [processData(row) for row in arcpy.da.SearchCursor(fs, fields)]
+def startPreDirProcess(fs, fields):
+    prefixDirValuesDirty = [processPreDirData(row) for row in arcpy.da.SearchCursor(fs, fields)]
     return prefixDirValuesDirty
+
+
+def startPreTypeProcess(fs, fields):
+    prefixDirValuesDirty = [processPreTypeData(row) for row in arcpy.da.SearchCursor(fs, fields)]
+    return prefixDirValuesDirty
+
+def startRoadNameProcess(fs, fields):
+    roadNames = [processRoadData(row) for row in arcpy.da.SearchCursor(fs, fields)]
+    return roadNames
+
+def cleanData(data):
+    if data is None:
+        return ""
+    elif len(data) > 0:
+        data = data.strip()
+    return data
+
+def getLegacyData(fs, fields):
+    LegacyStreetType = [row for row in arcpy.da.SearchCursor(fs, fields)]
+    
+def unique_values(table, field):  ##uses numpy
+    global LegacyStreetType
+    data = arcpy.da.TableToNumPyArray(table, [field])
+    holdArr = numpy.unique(data[field])
+    dataClean = [d for d in holdArr if len(d) > 0]
+    LegacyStreetType = dataClean
+    print(LegacyStreetType)
+    
     
     
     
--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-import arcpy
+import arcpy, sys
 from arcpy import env
 from handler import *
 
 
 
+
+# Setup Legacy Data
+AustinStreetShapefileReference = "C:/Users/Noe956/Documents/GIS/HC_IT/DataReference/AustinStreetsLine/AustinStreets.shp"
+
+unique_values(AustinStreetShapefileReference, 'street_typ')
+
+print("Donge Gathering Legacy Street Type")
+
+# Change Environment to the correct gis database clean up..
 env.workspace = "C:/Users/Noe956/Documents/GIS/HC_IT/GIS_DATA_Hidalgo.gdb"
 
 
-feature_address_points = "Address_Points";
+feature_address_points = "Address_Points"
 
-feature_road_centerlines = "Road_Centerlines";
+feature_road_centerlines = "Road_Centerlines"
 
 fields_address_points = ['fullname', 'objectid', 'prd']
 
@@ -17,32 +26,38 @@ update_address_points = ['prd', 'objectid']
 
 
 print("PARSING DIRTY DATA")
-test()
-# prefixDirValuesDirtyAddressPoints = startProcess(feature_address_points, ['fullname', 'objectid', 'prd'])
 
-prefixDirValuesDirtyRoadCenterlines = startProcess(feature_road_centerlines, ['fullname', 'objectid', 'PrefixType'])
+# prefixDirValuesDirtyAddressPoints = startPreDirProcess(feature_address_points, ['fullname', 'objectid', 'prd'])
+
+# Get Road Name
+roadNameDirty = startRoadNameProcess(feature_road_centerlines, ['objectid', 'fullname', 'PrefixDir', 'PrefixType'])
 #['fullname', 'objectid', 'PrefixDir'])
 
+print("done road name dirty")
+
 #Get Only the ones that have single letter and prd is empty
-hasSingleLetter = [i for i in prefixDirValuesDirtyRoadCenterlines if i.hasStType()]
+#hasSingleLetter = [i for i in prefixDirValuesDirtyRoadCenterlines if i.hasStType()]
 
 # get all the objects ids that specify the condition from hasSingleLetter..
-allObjectsSingleLetter = str([i.getObjectId() for i in hasSingleLetter]).replace("[", "(").replace("]", ")")
+allObjectsSingleLetter = str([i.getObjectId() for i in roadNameDirty]).replace("[", "(").replace("]", ")")
 
-print(len(hasSingleLetter))
 print(len(allObjectsSingleLetter))
 print("STARTING THE PROCESS")
+
+if len(allObjectsSingleLetter) == 2:
+    sys.exit("No Objects Ids")
+
 # Get all the rows that we need to update
-with arcpy.da.UpdateCursor(feature_road_centerlines, ['PrefixType', 'objectid'], where_clause="objectid in %s" % allObjectsSingleLetter) as cursor:
+with arcpy.da.UpdateCursor(feature_road_centerlines, ['StreetName', 'objectid'], where_clause="objectid in %s" % allObjectsSingleLetter) as cursor:
     #Loop through all the rows
     for row in cursor:
         #Search the prefixValue
-        indexValue, prefixValue = findValue(hasSingleLetter, row[1])
+        indexValue, roadValue = findRoadName(roadNameDirty, row[1])
         # if prefixvalue is not None
-        if prefixValue is not None and indexValue is not None:
-            row[0] = prefixValue
+        if roadValue is not None and indexValue is not None:
+            row[0] = roadValue
             cursor.updateRow(row) #finally update the row
-            hasSingleLetter.pop(indexValue)
+            roadNameDirty.pop(indexValue)
 
 print("Done Update Prefix")
 
