2dab91a56f95e2932be1b3ec34621127c7042852
Hector Chapa <roensebastian2015@gmail.com>
2023-01-29 12:44:31 -0600

    modifications for updates startup

--- handler.py
+++ handler.py
@@ -4,12 +4,12 @@ LegacyStreetType = []
 
 class AddressParser:
     # init constructor
-    def __init__(self, objectid, fullRoadName, preDir="", preType=""):
+    def __init__(self, objectid, fullRoadName, preDir="", stType=""):
         self.roadName = fullRoadName.strip()
         self.splitRoadArr = fullRoadName.split()
         self.splitLen = len(self.splitRoadArr)
         self.preDir = preDir.strip() if preDir is not None else ""
-        self.preType = preType.strip() if preType is not None else ""
+        self.stType = stType.strip() if stType is not None else ""
         self.objectid = objectid
         self.hasData = len(self.roadName) > 1
         self.isEmpty = len(self.roadName) == 0
@@ -26,7 +26,7 @@ class AddressParser:
     
     def hasStType(self):
         global LegacyStreetType
-        return self.hasData and  len(self.preType) == 0 and self.splitRoadArr[self.splitLen - 1] in LegacyStreetType
+        return self.hasData and  len(self.stType) == 0 and self.splitRoadArr[self.splitLen - 1] in LegacyStreetType
     
     def getIsEmpty(self):
         return self.isEmpty
@@ -41,20 +41,20 @@ class AddressParser:
         
     def getRoadName(self):
         results = ""
-        if len(self.preType) > 0 and len(self.preDir) > 0:
+        if len(self.stType) > 0 and len(self.preDir) > 0:
             start = 1
             end = self.splitLen - 1
             for index in range(start, end):
                 results += self.splitRoadArr[index] + " "
-        elif len(self.preType) == 0 and len(self.preDir) == 0:
+        elif len(self.stType) == 0 and len(self.preDir) == 0:
             start = 0
             end = self.splitLen
             for index in range(start, end):
                 results += self.splitRoadArr[index] + " "
-        elif len(self.preDir) == 0 and len(self.preType) > 0:
+        elif len(self.preDir) == 0 and len(self.stType) > 0:
             for index in range(self.splitLen - 1):
                 results += self.splitRoadArr[index] + " "
-        elif len(self.preDir) > 0 and len(self.preType) == 0:
+        elif len(self.preDir) > 0 and len(self.stType) == 0:
             start = 1
             end = self.splitLen
             for index in range(start, end):
@@ -70,16 +70,22 @@ def test():
     print(hello.getField())
 
 def processPreDirData(data):
-    return AddressParser(data[1], data[0], data[2], "")
+    return AddressParser(data[0], data[1], data[2], "")
 
-def processPreTypeData(data):
-    return AddressParser(data[1], data[0],"", data[2])
+def processStTypeData(data):
+    return AddressParser(data[0], data[1],"", data[2])
 
 
 def processRoadData(data):
     return AddressParser(data[0], data[1],data[2], data[3])
 
-def findValue(arr, item):
+def findPreDirValue(arr, item):
+    for i, val in enumerate(arr):
+        if val.getObjectId() == item:
+            return i, val.getPrefixDir()
+    return None, None
+
+def findStTypeValue(arr, item):
     for i, val in enumerate(arr):
         if val.getObjectId() == item:
             return i, val.getStType()
@@ -95,18 +101,63 @@ def findTest():
     return None, None
 
 
+def grabObjectIds(data):
+    allObjectsIdsForUpdate = str([i.getObjectId() for i in data]).replace("[", "(").replace("]", ")")
+    return allObjectsIdsForUpdate
+
 def startPreDirProcess(fs, fields):
     prefixDirValuesDirty = [processPreDirData(row) for row in arcpy.da.SearchCursor(fs, fields)]
-    return prefixDirValuesDirty
+    filterPreDirOnlyUpdates = [i for i in StreetTypeValuesDirty if i.hasSingle()]
+    objectIdsForUpdate = grabObjectIds(filterPreDirOnlyUpdates)
+    if len(objectIdsForUpdate) == 2:
+        return "Nothing To Update Street Type"
+    with arcpy.da.UpdateCursor(fs, ['PrefixDir', 'objectid'], where_clause="objectid in %s" % objectIdsForUpdate) as cursor:
+        #Loop through all the rows
+        for row in cursor:
+            #Search the prefixValue
+            indexValue, value = findPreDirValue(filterPreDirOnlyUpdates, row[1])
+            # if prefixvalue is not None
+            if value is not None and indexValue is not None:
+                row[0] = value
+                cursor.updateRow(row) #finally update the row
+                filterPreDirOnlyUpdates.pop(indexValue) # Remove from the list making it smaller
+    return "Pre Dir Update Completed"
 
 
-def startPreTypeProcess(fs, fields):
-    prefixDirValuesDirty = [processPreTypeData(row) for row in arcpy.da.SearchCursor(fs, fields)]
-    return prefixDirValuesDirty
+def startStTypeProcess(fs, fields):
+    StreetTypeValuesDirty = [processStTypeData(row) for row in arcpy.da.SearchCursor(fs, fields)]
+    filterStreetTypeOnlyValues = [i for i in StreetTypeValuesDirty if i.hasStType()]
+    objectIdsForUpdate = grabObjectIds(filterStreetTypeOnlyValues)
+    if len(objectIdsForUpdate) == 2:
+        return "Nothing To Update Street Type"
+    with arcpy.da.UpdateCursor(fs, ['StreetType', 'objectid'], where_clause="objectid in %s" % objectIdsForUpdate) as cursor:
+        #Loop through all the rows
+        for row in cursor:
+            #Search the prefixValue
+            indexValue, value = findStTypeValue(filterStreetTypeOnlyValues, row[1])
+            # if prefixvalue is not None
+            if value is not None and indexValue is not None:
+                row[0] = value
+                cursor.updateRow(row) #finally update the row
+                filterStreetTypeOnlyValues.pop(indexValue) # Remove from the list making it smaller
+    return "Street Type Completed"
 
 def startRoadNameProcess(fs, fields):
     roadNames = [processRoadData(row) for row in arcpy.da.SearchCursor(fs, fields)]
-    return roadNames
+    objectIdsForUpdate = grabObjectIds(roadNames)
+    if len(objectIdsForUpdate) == 2:
+        return "Nothing To Update Road Names"
+    with arcpy.da.UpdateCursor(fs, ['StreetName', 'objectid'], where_clause="objectid in %s" % objectIdsForUpdate) as cursor:
+        #Loop through all the rows
+        for row in cursor:
+            #Search the prefixValue
+            indexValue, value = findRoadName(roadNames, row[1])
+            # if prefixvalue is not None
+            if value is not None and indexValue is not None:
+                row[0] = value
+                cursor.updateRow(row) #finally update the row
+                roadNames.pop(indexValue) # Remove from the list making it smaller
+    return "Road Name Completed"
 
 def cleanData(data):
     if data is None:
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ AustinStreetShapefileReference = "C:/Users/Noe956/Documents/GIS/HC_IT/DataRefere
 
 unique_values(AustinStreetShapefileReference, 'street_typ')
 
-print("Donge Gathering Legacy Street Type")
+print("Done Gathering Legacy Street Type")
 
 # Change Environment to the correct gis database clean up..
 env.workspace = "C:/Users/Noe956/Documents/GIS/HC_IT/GIS_DATA_Hidalgo.gdb"
@@ -29,36 +29,18 @@ print("PARSING DIRTY DATA")
 
 # prefixDirValuesDirtyAddressPoints = startPreDirProcess(feature_address_points, ['fullname', 'objectid', 'prd'])
 
-# Get Road Name
-roadNameDirty = startRoadNameProcess(feature_road_centerlines, ['objectid', 'fullname', 'PrefixDir', 'PrefixType'])
-#['fullname', 'objectid', 'PrefixDir'])
+# Update the fields address
 
-print("done road name dirty")
+responseFromPreDir = startStTypeProcess(feature_road_centerlines, ['objectid', 'fullname', 'PrefixDir'])
+print(responseFromPreDir)
+responseFromStreetType = startStTypeProcess(feature_road_centerlines, ['objectid', 'fullname', 'StreetType'])
+print(responseFromStreetType)
 
-#Get Only the ones that have single letter and prd is empty
-#hasSingleLetter = [i for i in prefixDirValuesDirtyRoadCenterlines if i.hasStType()]
+#Road Names must be final from street type and pre directional. Is dependant on those values.
+responseRoadNames = startRoadNameProcess(feature_road_centerlines, ['objectid', 'fullname', 'PrefixDir', 'StreetType'])
+print(responseRoadNames)
 
-# get all the objects ids that specify the condition from hasSingleLetter..
-allObjectsSingleLetter = str([i.getObjectId() for i in roadNameDirty]).replace("[", "(").replace("]", ")")
 
-print(len(allObjectsSingleLetter))
-print("STARTING THE PROCESS")
-
-if len(allObjectsSingleLetter) == 2:
-    sys.exit("No Objects Ids")
-
-# Get all the rows that we need to update
-with arcpy.da.UpdateCursor(feature_road_centerlines, ['StreetName', 'objectid'], where_clause="objectid in %s" % allObjectsSingleLetter) as cursor:
-    #Loop through all the rows
-    for row in cursor:
-        #Search the prefixValue
-        indexValue, roadValue = findRoadName(roadNameDirty, row[1])
-        # if prefixvalue is not None
-        if roadValue is not None and indexValue is not None:
-            row[0] = roadValue
-            cursor.updateRow(row) #finally update the row
-            roadNameDirty.pop(indexValue)
-
-print("Done Update Prefix")
+print("Done Updating Everything")
 
 
